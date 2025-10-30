9542a4f338023c535a2062d1d7912c91fe71eddf
Ammar30113 <127151422+Ammar30113@users.noreply.github.com>
2025-10-30 11:10:45 -0400

    Fix import path for app module
    git add .
    git commit -m "Fix app import path"
    git push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import uvicorn
-from app import app
-
+from app.app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
